215fb7e466d1accb1665fb1311cab3e81db98a8c
James Newman <j@mesnewman.dev>
2022-05-17 23:22:32 +0100

    added hacky shitty colors

--- cu7/keymaps/mapped-cu7/keymap.c
+++ cu7/keymaps/mapped-cu7/keymap.c
@@ -1,22 +1,13 @@
-/*
-Copyright 2021 CapsUnlocked
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 #include QMK_KEYBOARD_H
 
-bool muted = false;
+bool glowRed = false;
+bool glowPurple = false;
+
+enum colors {
+  WHITE,
+  RED,
+  PURPLE
+};
 
 // Set RGB color
 void keyboard_post_init_user(void) {
@@ -25,20 +16,12 @@ void keyboard_post_init_user(void) {
   rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
 }
 
-// Macro keys
-enum macros {
-  M_DEAFEN,
-  M_MUTE,
-  M_CALL,
-};
-
 // Bindings
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
               KC_MUTE,
-    KC_MPRV,  KC_MPLY, KC_MNXT,
-    // M_DEAFEN, M_MUTE,  M_CALL
-    KC_F15, KC_F16,  KC_F17
+    KC_MPRV,  KC_MPLY,  KC_MNXT,
+    KC_F15,   KC_F16,   KC_F17
   )
 };
 
@@ -54,37 +37,65 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
   return true;
 }
 
-// Change RGB to red when muted
-void handle_mute_rgb(void) {
-  if (muted) {
-    rgblight_sethsv_noeeprom(HSV_WHITE);
-    muted = false;
-  } else {
-    rgblight_sethsv_noeeprom(HSV_RED);
-    muted = true;
+void ledWhite(void) {
+  rgblight_sethsv_noeeprom(HSV_WHITE);
+  rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
+}
+
+void ledRed(void) {
+  rgblight_sethsv_noeeprom(HSV_RED);
+  rgblight_mode_noeeprom(RGBLIGHT_MODE_ALTERNATING);
+}
+
+void ledPurple(void) {
+  rgblight_sethsv_noeeprom(HSV_PURPLE);
+  rgblight_mode_noeeprom(RGBLIGHT_MODE_ALTERNATING);
+}
+
+// Handle RGB based on button
+void handle_rgb_rgb(enum colors color) {
+  if (color == RED) {
+    if (glowRed == true && glowPurple == true) {
+      ledPurple();
+      glowRed = false;
+    } else if (glowRed == true && glowPurple== false) {
+      ledWhite();
+      glowRed = false;
+    } else {
+      ledRed();
+      glowRed = true;
+    }
+  } else if (color == PURPLE) {
+    if (glowPurple == true && glowRed == true) {
+      ledRed();
+      glowPurple = false;
+    } else if (glowPurple == true && glowRed == false) {
+      ledWhite();
+      glowPurple = false;
+    } else {
+      ledPurple();
+      glowPurple = true;
+    }
   }
 }
 
+
 // Execute Macros
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case M_DEAFEN:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LCTL(SS_LSFT("d")));
-        handle_mute_rgb();
-      }
-      break;
-    case M_MUTE:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LCTL(SS_LSFT("m")));
-        handle_mute_rgb();
-      }
-      break;
-    case M_CALL:
-      if (record->event.pressed) {
-        SEND_STRING(SS_LCTL(SS_LSFT(SS_LALT("v"))));
-      }
-      break;
+void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    switch (keycode)
+    {
+      case KC_F15:
+      case KC_F16:
+        handle_rgb_rgb(RED);
+        break;
+      
+      case KC_F17:
+        handle_rgb_rgb(PURPLE);
+        break;
+      
+      default:
+        break;
+    }
   }
-  return true;
 };
